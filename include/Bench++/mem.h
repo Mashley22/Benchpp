bb728615855116c13383a59836fe158ed1f57663
Mashley22 <ajkhsu22@gmail.com>
2025-11-18 22:24:26 +0000

    Stub for memory api

--- include/Bench++/mem.h
+++ include/Bench++/mem.h
@@ -1,7 +1,36 @@
 #ifndef BENCHPP_MEM_HPP
 #define BENCHPP_MEM_HPP
 
-namespace bpp {
+#include <cstddef>
+
+namespace benchpp {
+
+namespace mem {
+
+// Note these may be non negligble, so it is recommned not to mix these with speed benchmarks
+// May add more but keeping it simple for now
+
+[[nodiscard]]
+std::size_t
+get_peakVirtual(void);
+
+[[nodiscard]]
+std::size_t
+get_currentVirtual(void);
+
+[[nodiscard]]
+std::size_t
+get_peakPhysical(void);
+
+[[nodiscard]]
+std::size_t
+get_currentSwapped(void);
+
+[[nodiscard]]
+std::size_t
+get_currentPhysical(void);
+
+}
 
 }
 
