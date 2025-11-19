e041ee570cb18a05bbd1448d385611b582eb719a
Mashley22 <ajkhsu22@gmail.com>
2025-11-19 19:17:37 +0000

    Reworked the bash script to default to Ninja if available and the add_tests function is a bad name so renamed to add_unit_test

--- build/validation/build.sh
+++ build/validation/build.sh
@@ -1,1 +1,8 @@
-cmake ../.. -DCMAKE_EXPORT_COMPILE_COMMANDS=y -DBENCHPP_TEST=y
+EXTRA_ARGS="-DCMAKE_EXPORT_COMPILE_COMMANDS=y -DBENCHPP_TEST=y"
+
+
+if command -v ninja &> /dev/null; then
+    cmake ../.. -G Ninja "$EXTRA_ARGS"
+else
+    cmake ../.. "$EXTRA_ARGS"
+fi
