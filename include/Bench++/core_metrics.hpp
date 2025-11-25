126014f88339168b1af1f58844fd8c7b751c5261
Mashley22 <ajkhsu22@gmail.com>
2025-11-25 23:47:13 +0000

    Added the interface for the MetricCounter class

--- include/Bench++/core_metrics.hpp
+++ include/Bench++/core_metrics.hpp
@@ -2,6 +2,7 @@
 #define BENCHPP_CORE_METRICS_HPP
 
 #include <string>
+#include <cstdint>
 
 #define BENCHPP_METRIC_ALL_GENERATOR(macro) \
     macro(Metric::HW_CPU_CYCLES); \
@@ -33,6 +34,46 @@ enum class Metric {
 [[nodiscard]]
 std::string
 metric_toStr(const Metric&);
+
+namespace detail {
+#ifdef __linux__
+class LinuxCounter_impl {
+protected:
+  std::int64_t prev_val{0};
+  int fd{-1};
+  bool isRunning{false};
+};
+using Counter_impl = LinuxCounter_impl;
+#endif /* __linux__ */
+
+}
+
+template<Metric T_metric>
+class MetricCounter : detail::Counter_impl {
+public:
+
+  MetricCounter(void);
+
+  ~MetricCounter(void);
+
+  [[nodiscard]]
+  std::int64_t
+  read(void);
+
+  void
+  start(void);
+
+  void
+  stop(void);
+
+  [[nodiscard]]
+  Metric
+  metric(void) const noexcept {
+    return T_metric;
+  }
+private:
+  using m_ = detail::Counter_impl;
+};
 
 }
 
