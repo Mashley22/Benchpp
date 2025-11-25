3e29df1408a80273da95985a1f2ab56920ca2e24
Mashley22 <ajkhsu22@gmail.com>
2025-11-25 01:32:50 +0000

    Same with the statusfile stuff

---
+++ src/linux/statusFile.cpp
@@ -0,0 +1,70 @@
+#include "statusFile.hpp"
+
+#include <Bench++/linux/exception/mem.hpp>
+
+#include <string>
+
+#define LINUX_STATUS_FILE "/proc/self/status"
+
+namespace benchpp {
+
+namespace mem {
+
+namespace lnx {
+
+namespace priv {
+
+std::ifstream
+openStatusFile(void) {
+  std::ifstream statusFile;
+  statusFile.open(LINUX_STATUS_FILE);
+
+  if (!statusFile.is_open()) {
+    throw OpenStatusFileErr();
+  }
+
+  return statusFile;
+}
+
+std::size_t
+parseStatusFile(const std::string_view key) {
+  std::ifstream statusFile = openStatusFile();
+  std::string line;
+
+  while(std::getline(statusFile, line)) {
+    if (line.find(key) == 0) {
+      std::size_t num_start = line.find_first_of("0123456789", key.length()); // start after the length of the key
+
+      if (num_start == std::string::npos) {
+        throw LineParseErr();
+      }
+      
+      std::size_t num_end = line.find(' ', num_start); // space for the stuff
+
+      if (num_end == std::string::npos) {
+        throw LineParseErr();
+      }
+    
+      std::string num_str = line.substr(num_start, num_end);
+      std::size_t retval;
+      try {
+        retval = std::stoull(num_str);
+      }
+      catch(std::exception& e) {
+        throw StrTo_ull_Err();
+      }
+        
+      return retval;
+    }
+  }
+  
+  throw KeyNotFoundErr();
+}
+
+}
+
+}
+
+}
+
+}
