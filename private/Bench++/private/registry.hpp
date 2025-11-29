e301f6e36fff50e365fd09ae74e58680a94a643f
Mashley22 <ajkhsu22@gmail.com>
2025-11-29 17:17:57 +0000

    Did the implementation of the group runner class to abstraxt the proccess of running over a group

--- private/Bench++/private/registry.hpp
+++ private/Bench++/private/registry.hpp
@@ -22,6 +22,28 @@ run_benchmark(const std::string_view name);
 void
 run_benchmark(const std::string_view name, const std::size_t runNum);
 
+class GroupRunner {
+public:
+  GroupRunner() = delete;
+  GroupRunner(const std::string_view groupName) noexcept;
+  
+  /**@retval true if there is another benchmark in the group
+   * @retval false if there are no more benchmarks in the group
+   */
+  bool 
+  nextBenchmark(void) noexcept;
+
+  const BenchmarkInfo&
+  currentBenchmark(void) const noexcept;
+
+  void
+  reset(void) noexcept;
+  
+private:
+  std::string_view m_name;
+  std::size_t m_currentIdx{0};
+};
+
 }
 
 }
--- src/private/registry.cpp
+++ src/private/registry.cpp
@@ -2,6 +2,11 @@
 
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <cassert>
+#include <iostream>
+
+#define GROUP_RUNNER_INVALID_IDX std::numeric_limits<std::size_t>::max()
 
 namespace benchpp {
 
@@ -27,6 +32,20 @@ M_findBenchmark(const std::string_view name) {
   return it;
 }
 
+void
+M_run_benchmark_impl(const BenchmarkInfo& benchmark, const std::size_t runNum) {
+  std::cout << "Starting benchmark: " << benchmark.name
+            <<  " for " << runNum << " runs" << '\n';
+
+  for (std::size_t i = 0; i < runNum; i++) {
+    std::cout << "Run: " << runNum << '\n';
+    benchmark.function();
+  }
+
+  std::cout << "Finished benchmark: " << benchmark.name
+            <<  " did " << runNum << " runs" << '\n';
+}
+
 }
 
 void
@@ -36,28 +55,60 @@ add_benchmark(const BenchmarkInfo &info) {
 
 void
 run_benchmark(const BenchmarkInfo& benchmark, const std::size_t runNum) {
-  for (std::size_t i = 0; i < runNum; i++) {
-    benchmark.function();
-  }
+  M_run_benchmark_impl(benchmark, runNum);
 }
 
 void
 run_benchmark(const BenchmarkInfo& benchmark) {
-  run_benchmark(benchmark, benchmark.runNum);
+  M_run_benchmark_impl(benchmark, benchmark.runNum);
 }
 
 void
 run_benchmark(const std::string_view name) {
   std::vector<BenchmarkInfo>::const_iterator it = M_findBenchmark(name);
   
-  run_benchmark(*it);
+  M_run_benchmark_impl(*it, it->runNum);
 }
 
 void
 run_benchmark(const std::string_view name, const std::size_t runNum) {
   std::vector<BenchmarkInfo>::const_iterator it = M_findBenchmark(name);
   
-  run_benchmark(*it, runNum);
+  M_run_benchmark_impl(*it, runNum);
+}
+
+GroupRunner::GroupRunner(const std::string_view groupName) noexcept :
+  m_name(groupName) {}
+
+bool
+GroupRunner::nextBenchmark(void) noexcept {
+  assert(m_currentIdx != GROUP_RUNNER_INVALID_IDX);
+  
+  if (m_currentIdx != 0) {
+    m_currentIdx++;
+  }
+  
+  while (m_currentIdx < M_registeredBenchmarks.size()) {
+    if (M_registeredBenchmarks[m_currentIdx].name == m_name) {
+      return true;
+    }
+    m_currentIdx++;
+  }
+  
+  m_currentIdx = GROUP_RUNNER_INVALID_IDX;
+  return false;
+}
+
+const BenchmarkInfo&
+GroupRunner::currentBenchmark(void) const noexcept {
+  assert(m_currentIdx != GROUP_RUNNER_INVALID_IDX);
+  
+  return M_registeredBenchmarks[m_currentIdx];
+}
+
+void
+GroupRunner::reset(void) noexcept {
+  m_currentIdx = 0;
 }
 
 }
