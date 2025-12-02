8d16dce927310703dc9fd083167af8415582c9d8
Mashley22 <ajkhsu22@gmail.com>
2025-12-02 21:55:16 +0000

    Removed the launcher

--- cmake/add_benchmark.cmake
+++ cmake/add_benchmark.cmake
@@ -3,21 +3,6 @@ function(add_benchmark TARGET_BENCHMARK ITERATIONS)
   set_target_properties(${TARGET_BENCHMARK} PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/${TARGET_BENCHMARK}
   )
-  
-  set(LAUNCHER "launcher_${TARGET_BENCHMARK}")
-
-  add_executable(${LAUNCHER}
-    ${BENCHPP_ROOT_DIR}/src/launcher/launcher_main.cpp
-  )
-
-  set_target_properties(${LAUNCHER} PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/${TARGET_BENCHMARK}
-  )
-
-  target_compile_definitions(${LAUNCHER} PRIVATE 
-    BENCHMARK_NAME="./${TARGET_BENCHMARK}"
-    "ITERATION_NUM=$<1:${ITERATIONS}>"
-  )
 
   target_link_libraries(${TARGET_BENCHMARK}
     PRIVATE
